cccbfa746abb3aac7d96c2c843ca2af5ad63a1a9
trindadematheus97 <trindadedt3@gmail.com>
2022-08-09 23:56:14 -0300

    seed service

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -6,6 +6,7 @@ namespace SalesWebMvc.Models
 {
     public class Seller
     {
+       
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
@@ -19,15 +20,17 @@ namespace SalesWebMvc.Models
 
         }
 
-        public Seller(int id, string name, string email, double baseSalary, DateTime birthDate, Department department)
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
             Id = id;
             Name = name;
             Email = email;
-            BaseSalary = baseSalary;
             BirthDate = birthDate;
+            BaseSalary = baseSalary;
             Department = department;
         }
+
+
 
         public void AddSales(SalesRecord sr)
         {
