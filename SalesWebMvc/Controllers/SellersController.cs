3e1d6d0d19a6eeaff67ce13fe47c5bd20bb2d29b
trindadematheus97 <trindadedt3@gmail.com>
2022-08-11 23:28:17 -0300

    views

---
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
+using SalesWebMvc.Services;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SellersController : Controller
+    {
+        private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
+
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
+        {
+            _sellerService = sellerService;
+            _departmentService = departmentService;
+        }
+
+        public IActionResult Index()
+        {
+            var list = _sellerService.FindAll();
+            return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            var departments = _departmentService.FindAll();
+            var ViewModel = new SellerFormViewModel { Departments = departments };
+            return View(ViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc
 {
@@ -41,6 +42,8 @@ namespace SalesWebMvc
                     builder.MigrationsAssembly("SalesWebMvc")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
+            services.AddScoped<DepartmentService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
